e0eeb5d7d87bd9f1935ab6db8c582bfa87fde937
zaid <szaid5775@gmail.com>
2023-06-20 16:47:51 +0530

    new hai new

--- index.js
+++ index.js
@@ -91,12 +91,12 @@ app.post("/Signup", (req, res) => {
 });
 
 // Serve the static files for your React app
-app.use(express.static(path.join(__dirname, "../cipherscape")));
+// app.use(express.static(path.join(__dirname, "../cipherscape")));
 
-// For any other route, serve the index.html file
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../cipherscape/index.html"));
-});
+// // For any other route, serve the index.html file
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../cipherscape/index.html"));
+// });
 
 
 const PORT = process.env.PORT
