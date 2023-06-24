cbac2f10459078cc16c24f51ceed2f167cdf83ea
zaid <szaid5775@gmail.com>
2023-06-24 18:54:26 +0530

    waps

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ app.post("/Signup", (req, res) => {
 
 app.get('/user', (req, res) => {
   const { username } = req.session;
-  
+  console.log('Username:', username);
   PlayersModel.findOne({ username }, 'username email')
     .then((user) => {
       if (user) {
